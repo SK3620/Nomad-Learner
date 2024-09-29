0f0faee34d71e3f1c3ce69fca336629afd235979
SK3620 <k.n.t11193620@gmail.com>
2024-09-29 22:58:46 +0900

    Add: 区切り線を表示するカスタムViewファイルを追加

---
+++ Nomad-Learner/View/Components/Auth/SeparatorWithLabelView.swift
@@ -0,0 +1,61 @@
+//
+//  SeparatorWithLabelView.swift
+//  Nomad-Learner
+//
+//  Created by 鈴木 健太 on 2024/09/29.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+import UIKit
+import SnapKit
+
+class SeparatorWithLabelView: UIStackView {
+    
+    private let leftLineView = UIView().then {
+        $0.backgroundColor = .lightGray
+    }
+    
+    private let rightLineView = UIView().then {
+        $0.backgroundColor = .lightGray
+    }
+    
+    private let label = UILabel().then {
+        $0.textColor = .gray
+        $0.font = UIFont.systemFont(ofSize: UIConstants.Font.smallFont)
+        $0.textAlignment = .center
+    }
+    
+    init(text: String? = nil) {
+        super.init(frame: .zero)
+        label.text = text
+        setupUI()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        axis = .horizontal
+        alignment = .center
+        distribution = .fillEqually
+
+        addArrangedSubview(leftLineView)
+        // 中央にテキストを表示するか否か
+        if label.text != nil {
+            addArrangedSubview(label)
+        }
+        addArrangedSubview(rightLineView)
+        
+        leftLineView.snp.makeConstraints {
+            $0.height.equalTo(1.5)
+        }
+     
+        rightLineView.snp.makeConstraints {
+            $0.height.equalTo(1.5)
+        }
+    }
+}

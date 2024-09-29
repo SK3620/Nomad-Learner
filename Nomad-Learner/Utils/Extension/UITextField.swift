8f8472bb2e41f81db6b7ba67ca4ee638eac476ae
SK3620 <k.n.t11193620@gmail.com>
2024-09-29 22:56:00 +0900

    Add: フォント一括指定用＆キーボード操作用のUITextField拡張クラスを作成

---
+++ Nomad-Learner/Utils/Extension/UITextField.swift
@@ -0,0 +1,37 @@
+//
+//  TextField.swift
+//  Nomad-Learner
+//
+//  Created by 鈴木 健太 on 2024/09/29.
+//
+
+import UIKit
+
+extension UITextField {
+    
+    // フォントの一括指定
+    @objc var substituteFontName: String {
+        get {
+            return font!.fontName
+        }
+        set {
+            // 通常フォントのみ適用
+            if font!.fontName.range(of: "Medium") == nil {
+                font = UIFont(name: newValue, size: font!.pointSize)
+            }
+        }
+    }
+    
+    // キーボード閉じるボタンの追加
+    func addDoneToolbar(onDone: (target: Any, action: Selector)? = nil) {
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let image = UIImage(systemName: "chevron.down")
+        let doneButton = UIBarButtonItem(image: image, style: .done, target: onDone?.target, action: onDone?.action)
+        doneButton.tintColor = ColorCodes.primaryPurple.color()
+        toolbar.items = [spacer, doneButton]
+        self.inputAccessoryView = toolbar
+    }
+}

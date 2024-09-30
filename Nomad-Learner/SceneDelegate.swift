b031b8499750cecd88bf6a543bfe1b1c29a5c3c8
SK3620 <k.n.t11193620@gmail.com>
2024-09-30 12:07:08 +0900

    Update: アプリ起動時のrootViewを設定

--- Nomad-Learner/SceneDelegate.swift
+++ Nomad-Learner/SceneDelegate.swift
@@ -16,7 +16,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        // windowの生成
+        let window = UIWindow(windowScene: scene)
+        // ルートビューの指定
+        window.rootViewController = UINavigationController(rootViewController: AuthViewController())
+        // キーwindowの指定
+        window.makeKeyAndVisible()
+        // windowインスタンスが解放されないように引数に代入
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -46,7 +54,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-

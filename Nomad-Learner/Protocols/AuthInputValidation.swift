ce4c80ee494dc28acb6ae66cd042389b5dab07c9
SK3620 <k.n.t11193620@gmail.com>
2024-10-01 21:59:47 +0900

    Add: AuthInputValidationの追加とUILabelに対するリアクティブ拡張を実装

---
+++ Nomad-Learner/Protocols/AuthInputValidation.swift
@@ -0,0 +1,57 @@
+//
+//  AuthInputValidation.swift
+//  Nomad-Learner
+//
+//  Created by 鈴木 健太 on 2024/10/01.
+//
+
+import RxSwift
+import RxCocoa
+import UIKit
+
+enum MyError: Error {
+    case notAuth
+    case unknown
+}
+
+enum AuthInputValidation {
+    case ok(message: String)
+    case empty(message: String)
+    case validating
+    case failed(message: String)
+
+    var description: String {
+        switch self {
+        case let .ok(message) : return message
+        case let .empty(message) : return message
+        case .validating : return ""
+        case let .failed(message) : return message
+        }
+    }
+
+    var isValid: Bool {
+        switch self {
+        case .ok:
+            return true
+        default:
+            return false
+        }
+    }
+}
+
+extension Reactive<UILabel> where Base: UILabel {
+    var validationResult2: Binder<AuthInputValidation> {
+        return Binder.init(self.base, binding: { label, result in
+            label.text = result.description
+
+        })
+    }
+}
+
+extension Reactive<UILabel> where Base: UILabel {
+    var validationResult: Binder<AuthInputValidation> {
+        return Binder(base) { label, result in
+            label.text = result.description
+        }
+    }
+}

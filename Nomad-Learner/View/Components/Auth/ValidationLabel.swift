6f824bae3336712442d1f65c407b613174b86312
SK3620 <k.n.t11193620@gmail.com>
2024-10-03 23:25:49 +0900

    Add: バリデーションラベルの追加

---
+++ Nomad-Learner/View/Components/Auth/ValidationLabel.swift
@@ -0,0 +1,29 @@
+//
+//  ValidationLabel.swift
+//  Nomad-Learner
+//
+//  Created by 鈴木 健太 on 2024/10/03.
+//
+
+import UIKit
+import SnapKit
+
+class ValidationLabel: UILabel {
+
+    init() {
+        super.init(frame: .zero)
+        self.textColor = .red
+        self.font = UIFont.systemFont(ofSize: UIConstants.UILabel.fontSize)
+        self.text = "パスワードは8文字以上で入力してください"
+    }
+    
+    private func setupUI() {
+        self.snp.makeConstraints {
+            $0.height.equalTo(16)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

b1e7a4048c0146a96b4340938d0a883a36b3878d
SK3620 <k.n.t11193620@gmail.com>
2024-10-06 21:39:54 +0900

    Add: UIに影をつける汎用性のあるファイルを追加

--- Nomad-Learner/Utils/Extension/UIView.swift
+++ Nomad-Learner/Utils/Extension/UIView.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIView {
+    
+    func applyShadow(
+        color: UIColor = .black, // 影の色
+        opacity: Float = 0.2, // 透明度
+        offset: CGSize = CGSize(width: 0, height: 2), // オフセット
+        radius: CGFloat = 4 // ぼかし
+    ) {
+        self.layer.shadowColor = color.cgColor
+        self.layer.shadowOpacity = opacity
+        self.layer.shadowOffset = offset
+        self.layer.shadowRadius = radius
+        self.layer.masksToBounds = false
+    }
+}

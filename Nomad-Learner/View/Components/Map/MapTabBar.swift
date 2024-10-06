45a66f4b1536c4269aca413fd8d968ce843fbea6
SK3620 <k.n.t11193620@gmail.com>
2024-10-06 21:41:14 +0900

    Add: マップを表示する画面を追加＆TabBarを追加

--- Nomad-Learner/View/Components/Map/MapTabBar.swift
+++ Nomad-Learner/View/Components/Map/MapTabBar.swift
@@ -6,3 +6,95 @@
 //
 
 import Foundation
+import UIKit
+import Then
+import SnapKit
+
+class MapTabBar: UIView {
+    
+    // マップボタン
+    private lazy var mapItem: UIButton = MapTabBarItem(name: "map")
+    
+    // 飛行機（出発）ボタン
+    private lazy var airplaneItem: UIButton = MapTabBarItem(name: "airplane", color: .white).then {
+        $0.backgroundColor = ColorCodes.primaryPurple.color()
+        $0.layer.cornerRadius = UIConstants.Button.height / 2
+        // 縦向きの飛行機に調整
+        $0.imageView!.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi) * 2 * 270 / 360)
+        // 影を追加
+        $0.applyShadow(
+            color: .black,
+            opacity: 0.6,
+            offset: CGSize(width: 0.5, height: 4),
+            radius: 5
+        )
+    }
+    
+    // レポートボタン
+    private lazy var reportItem: UIButton = MapTabBarItem(name: "doc.plaintext")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.layer.cornerRadius = UIConstants.TabBarHeight.height / 2
+        self.backgroundColor = .white
+        // 影を追加
+        self.applyShadow(color: .black, opacity: 0.3, offset: CGSize(width: 1, height: 1), radius: 3)
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        
+        addSubview(mapItem)
+        addSubview(airplaneItem)
+        addSubview(reportItem)
+        
+        mapItem.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.size.equalTo(UIConstants.Button.height)
+            $0.right.equalTo(airplaneItem.snp.left).offset(-(UIConstants.Layout.extraLargePadding))
+        }
+        mapItem.imageView!.snp.makeConstraints {
+            $0.size.equalTo(UIConstants.Image.size)
+        }
+        
+        airplaneItem.snp.makeConstraints {
+            $0.size.equalTo(UIConstants.Button.height)
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview().offset(-(UIConstants.Layout.extraSmallPadding))
+        }
+        airplaneItem.imageView!.snp.makeConstraints {
+            $0.size.equalTo(UIConstants.Image.size)
+        }
+        
+        reportItem.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.size.equalTo(UIConstants.Button.height)
+            $0.left.equalTo(airplaneItem.snp.right).offset(UIConstants.Layout.extraLargePadding)
+        }
+        reportItem.imageView!.snp.makeConstraints {
+            $0.size.equalTo(UIConstants.Image.size)
+        }
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class MapTabBarItem: UIButton {
+    
+    init(name: String, color: UIColor = ColorCodes.primaryPurple.color()) {
+        super.init(frame: .zero)
+        
+        setImage(UIImage(systemName: name), for: .normal)
+        tintColor = color
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
--- Nomad-Learner/View/Pages/MapViewController.swift
+++ Nomad-Learner/View/Pages/MapViewController.swift
@@ -6,24 +6,57 @@
 //
 
 import UIKit
+import SwiftUI
 
 class MapViewController: UIViewController {
-
+    
+    private lazy var navigationBoxBar: NavigationBoxBar = NavigationBoxBar()
+    
+    // タブバー
+    private lazy var mapTabBar: MapTabBar = MapTabBar()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .yellow
+        
+        // UIのセットアップ
+        setupUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupUI() {
+        
+        // ナビゲーションバーの設定
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.title = "NavigationBar"
+        
+        view.addSubview(navigationBoxBar)
+        view.addSubview(mapTabBar)
+        
+        navigationBoxBar.snp.makeConstraints {
+            $0.width.top.equalToSuperview()
+            $0.height.equalTo(180)
+        }
+        
+        mapTabBar.snp.makeConstraints {
+            $0.height.equalTo(UIConstants.TabBarHeight.height)
+            $0.horizontalEdges.equalToSuperview().inset(UIConstants.Layout.standardPadding)
+            $0.bottom.equalToSuperview().inset(UIConstants.Layout.semiMediumPadding)
+        }
     }
-    */
-
 }
+
+struct ViewControllerPreview: PreviewProvider {
+    struct Wrapper: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> some UIViewController {
+            UINavigationController(rootViewController: AuthViewController())
+        }
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        }
+    }
+    static var previews: some View {
+        Wrapper()
+    }
+}
+

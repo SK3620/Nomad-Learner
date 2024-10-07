f7268cf1a321942f440f246e16c411fcdec5021d
SK3620 <k.n.t11193620@gmail.com>
2024-10-07 22:01:20 +0900

    Add: 各種ファイルの追加

---
+++ Nomad-Learner/View/Components/Map/LocationCategory.swift
@@ -0,0 +1,8 @@
+//
+//  LocationCategory.swift
+//  Nomad-Learner
+//
+//  Created by 鈴木 健太 on 2024/10/07.
+//
+
+import Foundation
---
+++ Nomad-Learner/View/Components/Map/LocationCategoryCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  LocationCategoryCollectionViewCell.swift
+//  Nomad-Learner
+//
+//  Created by 鈴木 健太 on 2024/10/07.
+//
+
+import Foundation

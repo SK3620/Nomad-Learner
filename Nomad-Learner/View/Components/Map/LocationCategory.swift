8a98faaddb6ab58b89f768810fa9117c354e42c1
SK3620 <k.n.t11193620@gmail.com>
2024-10-08 20:03:06 +0900

    Update: LocationCategoryItemを配列として定義

--- Nomad-Learner/View/Components/Map/LocationCategory.swift
+++ Nomad-Learner/View/Components/Map/LocationCategory.swift
@@ -7,32 +7,16 @@
 
 import Foundation
 
-enum LocationCategory: String, CaseIterable {
-    case mountain = "Mountain"
-    case book = "Book"
-    case leaf = "Leaf"
-    case person = "Person"
-    case airplane = "Airplane"
-    case tennisBall = "Tennis Ball"
-
-    var title: String {
-        return self.rawValue
-    }
-
-    var systemImageName: String {
-        switch self {
-        case .mountain:
-            return "mountain.2"
-        case .book:
-            return "book"
-        case .leaf:
-            return "leaf"
-        case .person:
-            return "person"
-        case .airplane:
-            return "airplane"
-        case .tennisBall:
-            return "tennisball"
-        }
-    }
+struct LocationCategoryItem {
+    let title: String
+    let systemImageName: String
+    
+    static let categories: [LocationCategoryItem] = [
+        LocationCategoryItem(title: "Mountain", systemImageName: "mountain.2"),
+        LocationCategoryItem(title: "Book", systemImageName: "book"),
+        LocationCategoryItem(title: "Leaf", systemImageName: "leaf"),
+        LocationCategoryItem(title: "Person", systemImageName: "person"),
+        LocationCategoryItem(title: "Airplane", systemImageName: "airplane"),
+        LocationCategoryItem(title: "Tennis Ball", systemImageName: "tennisball")
+    ]
 }

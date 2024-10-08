8436c44a48a4d1b85b0a6d3be2b2a6bf3c82cc83
SK3620 <k.n.t11193620@gmail.com>
2024-10-08 19:58:27 +0900

    Add: 各種ファイルを追加

---
+++ Nomad-Learner/Utils/Extension/Observable.swift
@@ -0,0 +1,8 @@
+//
+//  Observable.swift
+//  Nomad-Learner
+//
+//  Created by 鈴木 健太 on 2024/10/08.
+//
+
+import Foundation
---
+++ Nomad-Learner/View/Components/Map/LocationCategoryCollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationCategoryCollectionView.swift
+//  Nomad-Learner
+//
+//  Created by 鈴木 健太 on 2024/10/08.
+//
+
+import UIKit
+
+class LocationCategoryCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

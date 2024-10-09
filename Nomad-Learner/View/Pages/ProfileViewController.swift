d93c451b0ef72080f5bd41745d7253fac6273a27
SK3620 <k.n.t11193620@gmail.com>
2024-10-09 11:03:52 +0900

    Add: ProfileViewController, ProfileViewModelを追加

---
+++ Nomad-Learner/View/Pages/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  Nomad-Learner
+//
+//  Created by 鈴木 健太 on 2024/10/09.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Nomad-Learner/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileViewModel.swift
+//  Nomad-Learner
+//
+//  Created by 鈴木 健太 on 2024/10/09.
+//
+
+import Foundation

649db0544faf100b1d95a497c255f735bf9a909b
SK3620 <k.n.t11193620@gmail.com>
2024-10-10 16:57:46 +0900

    Add: プロフィール画面にカスタムナビゲーションバーを追加

--- Nomad-Learner/View/Components/Profile/ProfileNavigationBar.swift
+++ Nomad-Learner/View/Components/Profile/ProfileNavigationBar.swift
@@ -6,15 +6,48 @@
 //
 
 import UIKit
+import Then
 
 class ProfileNavigationBar: UINavigationBar {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // マップ画面へ戻るボタン
+    public lazy var closeButton = UIBarButtonItem(image: UIImage(systemName: "xmark"), style: .plain, target: nil, action: nil).then {
+        $0.tintColor = .white
     }
-    */
-
+    
+    // プロフィール編集ボタン
+    private lazy var editButton = UIBarButtonItem(image: UIImage(systemName: "pencil"), style: .plain, target: nil, action: nil).then {
+        $0.tintColor = .white
+    }
+        
+    private lazy var navigationItem = UINavigationItem().then {
+        $0.leftBarButtonItem = closeButton
+        $0.rightBarButtonItem = editButton
+        $0.title = "Profile"
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    private func setupUI() {
+        self.barTintColor = ColorCodes.primaryPurple.color()
+        self.backgroundColor = ColorCodes.primaryPurple.color()
+        self.tintColor = ColorCodes.primaryPurple.color()
+        self.clipsToBounds = true
+        self.layer.cornerRadius = 20
+        self.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        self.titleTextAttributes = [
+            .foregroundColor: UIColor.white
+        ]
+        
+        // ナビゲーションアイテムを設定
+        self.setItems([navigationItem], animated: false)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }

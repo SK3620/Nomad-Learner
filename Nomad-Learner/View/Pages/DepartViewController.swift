d68c73a55df3ac4313b7a9fc84d30e9c9cdf563e
SK3620 <k.n.t11193620@gmail.com>
2024-10-11 23:52:43 +0900

    Update: DepartViewを表示

--- Nomad-Learner/View/Pages/DepartViewController.swift
+++ Nomad-Learner/View/Pages/DepartViewController.swift
@@ -14,9 +14,17 @@ import SwiftUI
 
 class DepartViewController: UIViewController {
     
+    // 出発View
+    private let departView: DepartView = DepartView()
+    
+    // チケット画像
+    private let departImageView: UIImageView = UIImageView().then {
+        $0.image = UIImage(named: "ticket")
+    }
+    
     // 出発キャンセルボタン
     private let cancelButton: UIButton = UIButton(type: .system).then {
-        $0.setTitle("キャンセル", for: .normal)
+        $0.setTitle("Cancel", for: .normal)
         $0.titleLabel?.font = UIFont.systemFont(ofSize: UIConstants.TextSize.large)
         $0.tintColor = .red
         $0.layer.borderColor = UIColor.red.cgColor
@@ -40,12 +48,21 @@ class DepartViewController: UIViewController {
     }
     
     private func setupUI() {
+        view.backgroundColor = .white
         
+        view.addSubview(departView)
         view.addSubview(cancelButton)
+        
+        departView.snp.makeConstraints {
+            $0.height.equalToSuperview().multipliedBy(0.85)
+            $0.top.right.left.equalToSuperview()
+        }
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(touchedKnob(_:)))
+        departView.knobButton.addGestureRecognizer(panGesture)
         
         cancelButton.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.bottom.equalToSuperview().inset(UIConstants.Layout.standardPadding)
+            $0.top.equalTo(departView.snp.bottom).offset(UIConstants.Layout.extraLargePadding)
             $0.horizontalEdges.equalToSuperview().inset(UIConstants.Layout.extraLargePadding)
             $0.height.equalTo(60)
         }
@@ -60,6 +77,14 @@ class DepartViewController: UIViewController {
                 self.backToMapVC
             })
             .disposed(by: disposeBag)
+    }
+}
+
+extension DepartViewController {
+    
+    // つまみ（knobButton)をスライド
+    @objc func touchedKnob(_ sender: UIPanGestureRecognizer) {
+        departView.slideKnob(sender: sender)
     }
 }
 

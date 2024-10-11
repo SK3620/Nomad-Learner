7829f0432100841113129a164ee81901e64fd3cb
SK3620 <k.n.t11193620@gmail.com>
2024-10-11 23:45:28 +0900

    Add: 各種ファイルを追加＆画像を追加

---
+++ Nomad-Learner/View/Components/Depart/DepartView.swift
@@ -0,0 +1,20 @@
+//
+//  DepartView.swift
+//  Nomad-Learner
+//
+//  Created by 鈴木 健太 on 2024/10/11.
+//
+
+import UIKit
+
+class DepartView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Nomad-Learner/ViewModel/DepartViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DepartViewModel.swift
+//  Nomad-Learner
+//
+//  Created by 鈴木 健太 on 2024/10/11.
+//
+
+import Foundation

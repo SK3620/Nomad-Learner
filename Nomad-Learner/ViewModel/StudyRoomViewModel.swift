c711bd23a3d5de9cbb108d0359b133792d51cf26
SK3620 <k.n.t11193620@gmail.com>
2024-10-14 14:45:43 +0900

    Update: StudyRoomViewModelを実装し、プロファイルとメッセージのデータバインディングを追加

--- Nomad-Learner/ViewModel/StudyRoomViewModel.swift
+++ Nomad-Learner/ViewModel/StudyRoomViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+class StudyRoomViewModel {
+        
+    private let profileRelay: BehaviorRelay<[Profile]> = BehaviorRelay<[Profile]>(value: [])
+    private let messageRelay: BehaviorRelay<[Message]> = BehaviorRelay<[Message]>(value: [])
+    
+    // MARK: - Output 外部公開
+    var profiles: Driver<[Profile]> {
+        return profileRelay.asDriver()
+    }
+    var messages: Driver<[Message]> {
+        return messageRelay.asDriver()
+    }
+    
+    init() {
+        profileRelay.accept(Profile.profiles)
+        messageRelay.accept(Message.messages)
+    }
+}
+
+extension StudyRoomViewModel {
+    
+}

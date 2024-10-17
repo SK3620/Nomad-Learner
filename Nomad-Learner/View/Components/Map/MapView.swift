4cd3595aaf8d124ede262c7485d60333e728acdf
SK3620 <k.n.t11193620@gmail.com>
2024-10-17 12:49:37 +0900

    Clean: ファイル整理

---
+++ Nomad-Learner/View/Components/Map/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Nomad-Learner
+//
+//  Created by 鈴木 健太 on 2024/10/17.
+//
+
+import UIKit
+
+class MapView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

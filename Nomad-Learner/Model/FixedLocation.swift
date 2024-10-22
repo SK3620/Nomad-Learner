5432aff210cb897146cd581934d3f3d5af4ccdf0
SK3620 <k.n.t11193620@gmail.com>
2024-10-22 15:03:38 +0900

    Update: 画像をプリフェッチし、あらかじめ読み込んでおく

--- Nomad-Learner/Model/FixedLocation.swift
+++ Nomad-Learner/Model/FixedLocation.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import RealmSwift
+import Kingfisher
 
 /*
 struct FixedLocation {
@@ -26,10 +27,28 @@ struct LocationDetail {
 
 class FixedLocation: Object {
     @Persisted var locationId: String
-    @Persisted var name: String
+    @Persisted var location: String
     @Persisted var country: String
     @Persisted var region: String
     @Persisted var latitude: Double
     @Persisted var longitude: Double
     @Persisted var imageUrls: List<String>
+}
+
+extension FixedLocation {
+    // imageUrlsを配列に変換するメソッド
+    var imageUrlsArr: [String] {
+        return Array(imageUrls)
+    }
+    
+    // 画像をあらかじめ読み込んでおく
+   static func prefetchImages(from fixedLocations: [FixedLocation]) {
+        // 全てのimageUrlsをURL型に変換し、nilのURLは除外
+        let urls = fixedLocations
+            .flatMap { $0.imageUrlsArr }
+            .compactMap { URL(string: $0) }
+        // 画像を事前に読み込む
+        let prefetcher = ImagePrefetcher(urls: urls)
+        prefetcher.start()
+    }
 }
--- Nomad-Learner/Utils/Extension/UIImageView.swift
+++ Nomad-Learner/Utils/Extension/UIImageView.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import UIKit
+import AlamofireImage
+import Kingfisher
+
+extension UIImageView {
+    
+    func setImage(with urlString: String) {
+        guard let url = URL(string: urlString) else {
+            self.image = UIImage(named: "photo") // URLが不正の場合はデフォルトの画像
+            return
+        }
+        
+        // Kingfisherを使用して画像を非同期に設定
+        self.kf.setImage(with: url) { result in
+            switch result {
+            case .success(let value):
+                self.image = value.image
+            case .failure(let error):
+                print("Failed to load image, error: \(error)")
+                self.image = UIImage(named: "photo")
+            }
+        }
+    }
+}
+

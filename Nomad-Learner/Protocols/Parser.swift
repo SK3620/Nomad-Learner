dd7cd354af388fe7297520c9e3bd24dfa4ef9825
SK3620 <k.n.t11193620@gmail.com>
2024-10-22 14:56:57 +0900

    Add: Firebaseから取得したロケーション情報のパース処理を追加

--- Nomad-Learner/Protocols/Parser.swift
+++ Nomad-Learner/Protocols/Parser.swift
@@ -7,10 +7,30 @@
 
 import Foundation
 
-protocol ParseProtocol {
-    
+final class FixedLocationParser {
+    // FixedLocationをパース
+    static func parse(_ locationId: String, _ data: [String: Any]) -> FixedLocation? {
+        guard let location = data["location"] as? String,
+              let country = data["country"] as? String,
+              let region = data["region"] as? String,
+              let latitude = data["latitude"] as? Double,
+              let longitude = data["longitude"] as? Double,
+              let imageUrls = data["imageUrls"] as? [String] else {
+            return nil
+        }
+        
+        let locationObj = FixedLocation()
+        locationObj.locationId = locationId
+        locationObj.location = location
+        locationObj.country = country
+        locationObj.region = region
+        locationObj.latitude = latitude
+        locationObj.longitude = longitude
+        locationObj.imageUrls.append(objectsIn: imageUrls)
+        
+        return locationObj
+    }
 }
-
 
 
 

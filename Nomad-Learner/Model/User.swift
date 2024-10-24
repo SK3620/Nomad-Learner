1f4ab35a14f98a71452f173e8653a412d781b9b8
SK3620 <k.n.t11193620@gmail.com>
2024-10-24 23:28:52 +0900

    Update: UserモデルにprofileImageUrlを追加し、各プロパティに適切な初期値を設定

--- Nomad-Learner/Model/User.swift
+++ Nomad-Learner/Model/User.swift
@@ -8,14 +8,52 @@
 import Foundation
 
 struct User {
-    let userId: String
-    let username: String
-    let currentCoin: Int
-    let livingPlaceAndWork: String
-    let studyContent: String
-    let goal: String
-    let visitedCountries: Int
-    let weeklyTime: Int
-    let totalTime: Int
-    let visitedLocatioins: [VisitedLocation]
+    var username: String
+    var profileImageUrl: String
+    var currentCoin: Int
+    var livingPlaceAndWork: String
+    var studyContent: String
+    var goal: String
+    var visitedCountries: Int
+    var weeklyTime: Int
+    var totalTime: Int
+    
+    init(
+        username: String = "ー",
+        profileImageUrl: String = "",
+        currentCoin: Int = 100000,
+        livingPlaceAndWork: String = "ー",
+        studyContent: String = "ー",
+        goal: String = "ー",
+        visitedCountries: Int = 0,
+        weeklyTime: Int = 0,
+        totalTime: Int = 0
+    ) {
+        self.username = username
+        self.profileImageUrl = profileImageUrl
+        self.currentCoin = currentCoin
+        self.livingPlaceAndWork = livingPlaceAndWork
+        self.studyContent = studyContent
+        self.goal = goal
+        self.visitedCountries = visitedCountries
+        self.weeklyTime = weeklyTime
+        self.totalTime = totalTime
+    }
+}
+
+extension User {
+    /// Userのプロパティを辞書形式に変換します。
+    var toDictionary: [String: Any] {
+        return [
+            "username": username,
+            "profileImageUrl": profileImageUrl,
+            "currentCoin": currentCoin,
+            "livingPlaceAndWork": livingPlaceAndWork,
+            "studyContent": studyContent,
+            "goal": goal,
+            "visitedCountries": visitedCountries,
+            "weeklyTime": weeklyTime,
+            "totalTime": totalTime
+        ]
+    }
 }

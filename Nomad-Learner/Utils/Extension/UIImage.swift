7111fc33337f949aa88268ec0f05af9a920fad23
SK3620 <k.n.t11193620@gmail.com>
2024-11-29 23:18:22 +0900

    Add: ウォークスルー画面へ遷移するBarButtonItemのアイコン画像を追加＆アイコン画像の整理

---
+++ Nomad-Learner/Utils/Extension/UIImage.swift
@@ -0,0 +1,8 @@
+//
+//  UIImage.swift
+//  Nomad-Learner
+//
+//  Created by 鈴木 健太 on 2024/11/29.
+//
+
+import Foundation
---
+++ Nomad-Learner/View/Pages/WalkThroughViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WalkThroughViewController.swift
+//  Nomad-Learner
+//
+//  Created by 鈴木 健太 on 2024/11/29.
+//
+
+import UIKit
+
+class WalkThroughViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

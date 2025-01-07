803e56121e5d7faa5e43d6982ef12364aee9f6b2
SK3620 <k.n.t11193620@gmail.com>
2025-01-07 19:57:04 +0900

    Add: ウォークスルー画面のそれぞれのページにおけるテキストのデザインをカスタマイズ

--- Nomad-Learner/Utils/Extension/UILabel.swift
+++ Nomad-Learner/Utils/Extension/UILabel.swift
@@ -43,4 +43,48 @@ extension UILabel {
     func contentSizeHeight() -> CGFloat {
         return intrinsicContentSize.height
     }
+    
+    // テキスト全体のスタイル設定と特定の単語に色や影を適用
+    func createAttributedText(
+        text: String,
+        highlightedWords: [String] = [],
+        highlightColor: UIColor = ColorCodes.primaryPurple.color(),
+        font: UIFont,
+        lineSpacing: CGFloat = 6,
+        kern: CGFloat = 0.8
+    ) -> NSAttributedString {
+        let attributedString = NSMutableAttributedString(string: text)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = lineSpacing
+        paragraphStyle.alignment = .center
+        textAlignment = .center
+        numberOfLines = 0
+        
+        // 全体のフォントや間隔を設定
+        attributedString.addAttributes([
+            .font: font,
+            .kern: kern,
+            .paragraphStyle: paragraphStyle
+        ], range: NSRange(location: 0, length: text.count))
+        
+        // 特定の文字列の色と下線を変更
+        highlightedWords.forEach { word in
+            let regex = try! NSRegularExpression(pattern: NSRegularExpression.escapedPattern(for: word))
+            let matches = regex.matches(in: text, range: NSRange(location: 0, length: text.utf16.count))
+            
+            matches.forEach { match in
+                let range = match.range
+                let shadow = NSShadow()
+                shadow.shadowColor = UIColor.orange // ぼやけた影の色
+                shadow.shadowBlurRadius = 10 // ぼやけ具合
+                
+                attributedString.addAttributes([
+                    .foregroundColor: highlightColor,
+                    .shadow: shadow,
+                ], range: range)
+            }
+        }
+        
+        return attributedString
+    }
 }
